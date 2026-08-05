02191796d8190ea74461bde9ef08513a4a19ede1
meethatkar <meethatkar000@gmail.com>
2026-08-05 23:17:28 +0530

    feat:update global application layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Navbar from './shared/components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-slate-950 text-slate-100 flex flex-col">
       <Navbar />
-      <Outlet />
+      <div className="flex-1 bg-slate-950">
+        <Outlet />
+      </div>
     </div>
   )
 }
