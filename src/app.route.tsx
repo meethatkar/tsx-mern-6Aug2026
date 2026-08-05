cc6d3b5d1a0e70b415997e455002e43dd8c948d1
meethatkar <meethatkar000@gmail.com>
2026-08-05 11:41:40 +0530

    feat: configured router and added tailwindcss

---
+++ src/app.route.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
+import Characters from "./features/characters/pages/Characters";
+import App from "./App";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/characters",
+    element: <Characters />
+  },
+])
