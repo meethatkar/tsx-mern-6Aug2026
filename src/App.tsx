6a074401dcb79d630a3984aec0997a1ed405b8b8
meethatkar <meethatkar000@gmail.com>
2026-08-06 16:50:09 +0530

    fix: removed Outlet route nad kept Character components as defualt main component on home home.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from 'react-router-dom'
 import Navbar from './shared/components/Navbar'
+import Characters from './features/characters/pages/Characters'
 
 const App = () => {
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100 flex flex-col">
       <Navbar />
       <div className="flex-1 bg-slate-950">
-        <Outlet />
+        <Characters />
       </div>
     </div>
   )
